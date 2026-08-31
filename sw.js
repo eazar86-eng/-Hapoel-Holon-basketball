23408099b92da666d4634b38404c830f65fccd91
eazar86-eng <eazar86@gmail.com>
2026-08-31 17:57:03 +0300

    Add PWA service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='avi-holon-v1';
+const CORE=['./','./index.html','./manifest.webmanifest','./icon.svg','./week-03.html','./camp.html','./birthdays.html','./attendance.html','./messages.html','./team.html'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response;}).catch(()=>caches.match(event.request).then(r=>r||caches.match('./index.html'))));});
